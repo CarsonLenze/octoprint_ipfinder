9970991a3d93f3afcb0d9b8dfab4e1106e23f39f
CarsonLenze <carsonlenze@gmail.com>
2022-03-24 11:34:44 -0400

    t

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -4,46 +4,36 @@ from __future__ import absolute_import
 import octoprint.plugin
 import socket, requests
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
+class IPFinderPlugin(octoprint.plugin.StartupPlugin,
                        octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.SettingsPlugin,
                        octoprint.plugin.AssetPlugin):
+
 	def on_after_startup(self):
+		send(self._settings.get(["url"]))
 		self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
-		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		s.connect(("8.8.8.8", 80))
-		local_ip = s.getsockname()[0]
-		s.close()
-		public_ip = requests.get('https://api.ipify.org').content.decode('utf8')
-
-		requests.post(self._settings.get(["url"]), json = {'local': local_ip, 'public': public_ip })
-
-	def get_settings_defaults(self):
-		return dict(url="https://octoprint.carsons.site/")
 
 	def get_template_configs(self):
 		return [
 			dict(type="settings", custom_bindings=False)
 		]
 
+	def get_settings_defaults(self):
+		return dict(url="https://octoprint.carsons.site/")
+
 	def on_settings_save(self,data):
 		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
-		self._logger.info(data['url'])
+		send(data['url'])
+
+	def send(url):
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		s.connect(("8.8.8.8", 80))
 		local_ip = s.getsockname()[0]
 		s.close()
 		public_ip = requests.get('https://api.ipify.org').content.decode('utf8')
+		requests.post(url, json = {'local': local_ip, 'public': public_ip })
+		self._logger.info("IP sent (url: %s)" % url)
 
-		requests.post(data['url'], json = {'local': local_ip, 'public': public_ip })
-
-	def get_assets(self):
-		return dict(
-			js=["js/helloworld.js"],
-			css=["css/helloworld.css"],
-			less=["less/helloworld.less"]
-		)
-
-__plugin_name__ = "Hello World"
+__plugin_name__ = "IP Finder"
 __plugin_pythoncompat__ = ">=2.7,<4"
-__plugin_implementation__ = HelloWorldPlugin()
+__plugin_implementation__ = IPFinderPlugin()
--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "helloworld"
+plugin_identifier = "ipfinder"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_helloworld"
+plugin_package = "octoprint_ipfinder"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Helloworld"
+plugin_name = "IP Finder"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """A quick "Hello World" example plugin for OctoPrint"""
+plugin_description = "Sends your servers ip to the given url so that you can find your servers ip without a screen"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Your Name"
+plugin_author = "Carson Lenze"
 
 # The plugin's author's mail address.
-plugin_author_email = "you@somewhere.net"
+plugin_author_email = "carsonlenze@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/yourGithubName/OctoPrint-Helloworld"
+plugin_url = "https://github.com/CarsonLenze/Plugin-Examples"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
